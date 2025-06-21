b3c65f8ff658c952dced0f03b22c7b70484e4101
BugsDaffyTaz <periwinklemohite@gmail.com>
2025-06-21 11:58:32 +0530

    Added summary chart at the end

--- countries.py
+++ countries.py
@@ -1,208 +1,11 @@
 import random
+from country_data import country_capital
 
-country_capital = {
-    "Afghanistan": "Kabul",
-    "Albania": "Tirana (Tirane)",
-    "Algeria": "Algiers",
-    "Andorra": "Andorra la Vella",
-    "Angola": "Luanda",
-    "Antigua and Barbuda": "Saint John's",
-    "Argentina": "Buenos Aires",
-    "Armenia": "Yerevan",
-    "Australia": "Canberra",
-    "Austria": "Vienna",
-    "Azerbaijan": "Baku",
-    "Bahamas": "Nassau",
-    "Bahrain": "Manama",
-    "Bangladesh": "Dhaka",
-    "Barbados": "Bridgetown",
-    "Belarus": "Minsk",
-    "Belgium": "Brussels",
-    "Belize": "Belmopan",
-    "Benin": "Porto Novo",
-    "Bhutan": "Thimphu",
-    "Bolivia": "La Paz (administrative), Sucre (official)",
-    "Bosnia and Herzegovina": "Sarajevo",
-    "Botswana": "Gaborone",
-    "Brazil": "Brasilia",
-    "Brunei": "Bandar Seri Begawan",
-    "Bulgaria": "Sofia",
-    "Burkina Faso": "Ouagadougou",
-    "Burundi": "Gitega",
-    "Cambodia": "Phnom Penh",
-    "Cameroon": "Yaounde",
-    "Canada": "Ottawa",
-    "Cape Verde": "Praia",
-    "Central African Republic": "Bangui",
-    "Chad": "N'Djamena",
-    "Chile": "Santiago",
-    "China": "Beijing",
-    "Colombia": "Bogota",
-    "Comoros": "Moroni",
-    "Congo, Democratic Republic of the": "Kinshasa",
-    "Congo, Republic of the": "Brazzaville",
-    "Costa Rica": "San Jose",
-    "Côte d'Ivoire (Ivory Coast)": "Yamoussoukro",
-    "Croatia": "Zagreb",
-    "Cuba": "Havana",
-    "Cyprus": "Nicosia",
-    "Czech Republic (Czechia)": "Prague",
-    "Denmark": "Copenhagen",
-    "Djibouti": "Djibouti",
-    "Dominica": "Roseau",
-    "Dominican Republic": "Santo Domingo",
-    "East Timor": "Dili",
-    "Ecuador": "Quito",
-    "Egypt": "Cairo",
-    "El Salvador": "San Salvador",
-    "England": "London",
-    "Equatorial Guinea": "Malabo",
-    "Eritrea": "Asmara",
-    "Estonia": "Tallinn",
-    "Eswatini (Swaziland)": "Mbabane",
-    "Ethiopia": "Addis Ababa",
-    "Federated States of Micronesia": "Palikir",
-    "Fiji": "Suva",
-    "Finland": "Helsinki",
-    "France": "Paris",
-    "Gabon": "Libreville",
-    "Gambia": "Banjul",
-    "Georgia": "Tbilisi",
-    "Germany": "Berlin",
-    "Ghana": "Accra",
-    "Greece": "Athens",
-    "Grenada": "Saint George's",
-    "Guatemala": "Guatemala City",
-    "Guinea": "Conakry",
-    "Guinea-Bissau": "Bissau",
-    "Guyana": "Georgetown",
-    "Haiti": "Port au Prince",
-    "Honduras": "Tegucigalpa",
-    "Hungary": "Budapest",
-    "Iceland": "Reykjavik",
-    "India": "New Delhi",
-    "Indonesia": "Jakarta",
-    "Iran": "Tehran",
-    "Iraq": "Baghdad",
-    "Ireland": "Dublin",
-    "Israel": "Jerusalem",
-    "Italy": "Rome",
-    "Jamaica": "Kingston",
-    "Japan": "Tokyo",
-    "Jordan": "Amman",
-    "Kazakhstan": "Astana",
-    "Kenya": "Nairobi",
-    "Kiribati": "Tarawa Atoll",
-    "Kosovo": "Pristina",
-    "Kuwait": "Kuwait City",
-    "Kyrgyzstan": "Bishkek",
-    "Laos": "Vientiane",
-    "Latvia": "Riga",
-    "Lebanon": "Beirut",
-    "Lesotho": "Maseru",
-    "Liberia": "Monrovia",
-    "Libya": "Tripoli",
-    "Liechtenstein": "Vaduz",
-    "Lithuania": "Vilnius",
-    "Luxembourg": "Luxembourg",
-    "Madagascar": "Antananarivo",
-    "Malawi": "Lilongwe",
-    "Malaysia": "Kuala Lumpur",
-    "Maldives": "Male",
-    "Mali": "Bamako",
-    "Malta": "Valletta",
-    "Marshall Islands": "Majuro",
-    "Mauritania": "Nouakchott",
-    "Mauritius": "Port Louis",
-    "Mexico": "Mexico City",
-    "Moldova": "Chisinau",
-    "Monaco": "Monaco",
-    "Mongolia": "Ulaanbaatar",
-    "Montenegro": "Podgorica",
-    "Morocco": "Rabat",
-    "Mozambique": "Maputo",
-    "Myanmar (Burma)": "Nay Pyi Taw",
-    "Namibia": "Windhoek",
-    "Nauru": "No official capital",
-    "Nepal": "Kathmandu",
-    "Netherlands": "Amsterdam",
-    "New Zealand": "Wellington",
-    "Nicaragua": "Managua",
-    "Niger": "Niamey",
-    "Nigeria": "Abuja",
-    "North Korea": "Pyongyang",
-    "North Macedonia (Macedonia)": "Skopje",
-    "Northern Ireland": "Belfast",
-    "Norway": "Oslo",
-    "Oman": "Muscat",
-    "Pakistan": "Islamabad",
-    "Palau": "Melekeok",
-    "Palestine": "Jerusalem",
-    "Panama": "Panama City",
-    "Papua New Guinea": "Port Moresby",
-    "Paraguay": "Asuncion",
-    "Peru": "Lima",
-    "Philippines": "Manila",
-    "Poland": "Warsaw",
-    "Portugal": "Lisbon",
-    "Qatar": "Doha",
-    "Romania": "Bucharest",
-    "Russia": "Moscow",
-    "Rwanda": "Kigali",
-    "Saint Kitts and Nevis": "Basseterre",
-    "Saint Lucia": "Castries",
-    "Saint Vincent and the Grenadines": "Kingstown",
-    "Samoa": "Apia",
-    "San Marino": "San Marino",
-    "Sao Tome and Principe": "Sao Tome",
-    "Saudi Arabia": "Riyadh",
-    "Scotland": "Edinburgh",
-    "Senegal": "Dakar",
-    "Serbia": "Belgrade",
-    "Seychelles": "Victoria",
-    "Sierra Leone": "Freetown",
-    "Singapore": "Singapore",
-    "Slovakia": "Bratislava",
-    "Slovenia": "Ljubljana",
-    "Solomon Islands": "Honiara",
-    "Somalia": "Mogadishu",
-    "South Africa": "Pretoria, Bloemfontein, Cape Town",
-    "South Korea": "Seoul",
-    "South Sudan": "Juba",
-    "Spain": "Madrid",
-    "Sri Lanka": "Sri Jayawardenapura Kotte",
-    "Sudan": "Khartoum",
-    "Suriname": "Paramaribo",
-    "Sweden": "Stockholm",
-    "Switzerland": "Bern",
-    "Syria": "Damascus",
-    "Taiwan": "Taipei",
-    "Tajikistan": "Dushanbe",
-    "Tanzania": "Dodoma",
-    "Thailand": "Bangkok",
-    "Togo": "Lome",
-    "Tonga": "Nuku'alofa",
-    "Trinidad and Tobago": "Port of Spain",
-    "Tunisia": "Tunis",
-    "Türkiye (Turkey)": "Ankara",
-    "Turkmenistan": "Ashgabat",
-    "Tuvalu": "Funafuti",
-    "Uganda": "Kampala",
-    "Ukraine": "Kyiv or Kiev",
-    "United Arab Emirates": "Abu Dhabi",
-    "United Kingdom": "London",
-    "United States": "Washington D.C.",
-    "Uruguay": "Montevideo",
-    "Uzbekistan": "Tashkent",
-    "Vanuatu": "Port Vila",
-    "Vatican City": "Vatican City",
-    "Venezuela": "Caracas",
-    "Vietnam": "Hanoi",
-    "Wales": "Cardiff",
-    "Yemen": "Sana'a",
-    "Zambia": "Lusaka",
-    "Zimbabwe": "Harare"
-}
+score = 0
+questions_attempted = 0
+correct_ans = 0
+wrong_ans = 0
+
 
 def get_random_country():
     country = random.choice(list(country_capital.keys()))
@@ -215,10 +18,14 @@ def capitals_game():
     print("Rules: " \
           "Type 'hint' for a clue \n"
           "Press '1' to know your final score or to exit the game."
-            )
+            "+2 points for getting an answer right without hints" \
+            "+1 point for getting an answer right with a hint" \
+            "0 points for wrong answers")
 
     score = 0
     while True:
+        global questions_attempted, correct_ans, wrong_ans
+        questions_attempted +=1
         country, correct_capital = get_random_country()
         print(f"\nCountry: {country}")
         guess = input("Guess the capital: ").strip()
@@ -226,11 +33,16 @@ def capitals_game():
 ###########score/exit
         if guess.lower() == '1':
           print(f"\nYour final score: {score}")
+          print(f"Questions attempted: {questions_attempted}")
+          print(f"Correct Answers: {correct_ans}")
+          print(f"Wrong Answers: {wrong_ans}")
+
           break
 
 ##################correct
         if guess.lower() == correct_capital.lower():
             score += 2
+            correct_ans +=1
             print("That is the correct answer.")
 
         elif guess.lower() == "hint":
@@ -238,10 +50,13 @@ def capitals_game():
             retry = input("Try again: ").strip()
             if retry.lower() == correct_capital.lower():
                 score +=1
+                correct_ans +=1
                 print("Great going, that's correct!")
             else: 
+                wrong_ans +=1
                 print("That's the wrong answer.")
         else: 
+            wrong_ans +=1
             print("That's the wrong answer.")
                 
     
