45017af7714e1e32457e09326cba6568171943ed
Kim <periwinklemohite@gmail.com>
2025-06-22 21:07:08 +0530

    Update countries.py

--- countries.py
+++ countries.py
@@ -19,12 +19,12 @@ def normalise_text(guess):
 def capitals_game():
     print("Let's guess the capitals of some countries")
     print("Try guessing the capital of each country, your score will be shown at the end.")
-    print("Rules: " \
+    print("Rules: \n" \
           "Type 'hint' for a clue \n"
           "Press '1' to know your final score or to exit the game.\n"
-            "+1 point for correct answers.\n" \
-            "0 points for wrong answers.\n"
-            "A timer would also be started in the beginning. \n")
+          "+1 point for correct answers.\n" 
+          "0 points for wrong answers.\n"
+          "A timer would also be started in the beginning. \n")
     
 
 
@@ -36,21 +36,23 @@ def capitals_game():
         print(f"\nCountry: {country}")
         guess = input("Guess the capital: ").strip()
 
-###########score/exit
+###########  score/exit  ##############
+        
         if guess.lower() == '1':
           
           end_time = time.time()
           total_time = round(end_time - start_time, 2)
-          
-          print(f"\nYour final score: {score}/{questions_attempted}")
-          print(f"\nTime taken: {total_time} seconds")
-          print(f"\nQuestions attempted: {questions_attempted}")
-          print(f"\nCorrect Answers: {correct_ans}")
-          print(f"\nWrong Answers: {wrong_ans}")
+            
+          print(f"\n🏁 Game Over!")
+          print(f"\n📈 Your final score: {score}/{questions_attempted}")
+          print(f"\n⏱️ Time taken: {total_time} seconds")
+          print(f"\n🎯Questions attempted: {questions_attempted}")
+          print(f"\n✅ Correct Answers: {correct_ans}")
+          print(f"\n❌ Wrong Answers: {wrong_ans}")
 
           break 
 
-##################correct
+############  correct or not ###########
 
         possible_answers = [
                     normalise_text(part.split('(')[0])
@@ -61,7 +63,7 @@ def capitals_game():
         if user_input in possible_answers:
             score += 1
             correct_ans +=1
-            print("That is the correct answer.")
+            print("✅ That is the correct answer.")
 
         elif guess.lower() == "hint":
             print(f"Hint: Starts with {correct_capital[0]} and has {len(correct_capital)} letters")
@@ -71,7 +73,7 @@ def capitals_game():
             if normalise_text(retry) in possible_answers:
                 score +=1
                 correct_ans +=1
-                print("Great going, that's correct!")
+                print("✅ Great going, that's correct!")
 
             elif retry.lower() == "1":
                 print(f"\nYour final score: {score}/{questions_attempted}")
@@ -83,13 +85,13 @@ def capitals_game():
 
             else: 
                 wrong_ans +=1
-                print(f"That's the wrong answer. The correct answer is {correct_capital.title()}.")
+                print(f"❌ That's the wrong answer. The correct answer is {correct_capital.title()}.")
         else: 
             wrong_ans +=1
-            print("That's the wrong answer.")
+            print("❌ That's the wrong answer. The correct answer is {correct_capital.title()}.")
                 
 
-
+##### file output run ######
 
 
 if __name__ == "__main__":
